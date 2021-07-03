c9460579e69a7395d8183dabe54f1c947b2ab34b
Mary <Mary@LAPTOP-936AEDSU>
2021-07-03 08:58:27 +0200

    Soluzione

--- Lab03_SpellChecker/src/main/java/it/polito/tdp/spellchecker/EntryPoint.java
+++ Lab03_SpellChecker/src/main/java/it/polito/tdp/spellchecker/EntryPoint.java
@@ -1,7 +1,7 @@
 package it.polito.tdp.spellchecker;
 
 import javafx.application.Application;
-import static javafx.application.Application.launch;
+import it.polito.tdp.spellchecker.model.Dictionary;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -12,11 +12,15 @@ public class EntryPoint extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Scene.fxml"));
-        
+
+    	FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Scene.fxml"));
+        Parent root = loader.load();
         Scene scene = new Scene(root);
-        scene.getStylesheets().add("/styles/Styles.css");
         
+        Dictionary model = new Dictionary();
+    	FXMLController controller  = loader.getController();
+        controller.setModel(model);
+      
         stage.setTitle("JavaFX and Maven");
         stage.setScene(scene);
         stage.show();
